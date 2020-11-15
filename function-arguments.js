099f4c366d464d12804edb5bc79448938fea5195
Krystle B Co <imkrystleco@gmail.com>
2020-11-15 09:23:12 +0000

    Commit with NodeSchool progress

---
+++ function-arguments.js
@@ -0,0 +1,4 @@
+function math(add, subtract, divide){
+  return total = ((subtract * divide) + add);
+}
+console.log(math(53, 61, 67));
---
+++ functions.js
@@ -0,0 +1,4 @@
+function eat(food) {
+ return food = food + ' tasted really good.'
+}
+console.log(eat('bananas'))
